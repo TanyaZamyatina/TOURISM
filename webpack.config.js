4b49bde194acd86cff4473accd8c0356fed656b0
Tanya Zamyatina <byc14@mail.ru>
2023-07-30 20:20:43 +0300

    add gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
-    publicPath: `/TOURISM/`,
     mode: 'development',
     entry: path.resolve(__dirname, 'src/js/main.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+        publicPath: `/TOURISM/`,
     },
     module: {
         rules: [
